12c2208e04ba545206621d7252777c4aba6844e9
Hemanth-kumar23 <hemantkumarr365@gmail.com>
2025-01-20 21:15:49 +0530

    dsa_java

---
+++ selectionShort.java
@@ -0,0 +1,15 @@
+public class sele {
+    public static void printArray(int arr[]){
+        for(int i = 0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+        System.out.println();
+    }
+   
+   
+    public static void main(String[] args) {
+        int arr[] = {2,3,5,4,6};
+
+    }
+    
+}
