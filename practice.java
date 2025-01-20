fa3db0f765d5dd35bd2e04efb71d2ea9b86bf7eb
Hemanth-kumar23 <hemantkumarr365@gmail.com>
2025-01-20 21:37:25 +0530

    pracitice

---
+++ practice.java
@@ -0,0 +1,26 @@
+public class practice {
+    public static void printArray(int arr[]){
+        for(int i = 0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {8 , 2 , 5, 6, 0};
+        // bubble short
+        for(int i = 0;i<arr.length-1;i++){
+            for(int j = 0; j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    // swap
+                    int tem = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tem;                }
+            }
+        }
+
+        printArray(arr);
+    }
+}
+    
+
