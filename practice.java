a55b1e059414ff8eeab96a097da4a483ea3ddf12
Hemanth-kumar23 <hemantkumarr365@gmail.com>
2025-01-22 20:23:38 +0530

    new question added

--- practice.java
+++ practice.java
@@ -1,26 +1,52 @@
-public class practice {
-    public static void printArray(int arr[]){
-        for(int i = 0;i<arr.length;i++){
-            System.out.println(arr[i]+" ");
 
+// public class practice {
+//     public static void printArray(int arr[]) {
+//         for (int i = 0; i < arr.length; i++) {
+//             System.out.println(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String[] args) {
+//         int arr[] = {8, 2, 5, 6, 0};
+//         // bubble short
+//         for (int i = 0; i < arr.length - 1; i++) {
+//             for (int j = 0; j < arr.length - i - 1; j++) {
+//                 if (arr[j] > arr[j + 1]) {
+//                     // swap
+//                     int tem = arr[j];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = tem;
+//                 }
+//             }
+//         }
+//         printArray(arr);
+//     }
+// }
+public class practice {
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
-    public static void main(String[] args) {
-        int arr[] = {8 , 2 , 5, 6, 0};
-        // bubble short
-        for(int i = 0;i<arr.length-1;i++){
-            for(int j = 0; j<arr.length-i-1;j++){
-                if(arr[j]>arr[j+1]){
-                    // swap
-                    int tem = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = tem;                }
-            }
-        }
 
+    public static void main(String[] args) {
+
+        int arr[] = {5, 2, 9, 1, 5, 6};
+        // bubble short 
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+
+        }
         printArray(arr);
     }
 }
-    
-
